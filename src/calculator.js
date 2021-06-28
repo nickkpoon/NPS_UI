9087e46acdb068fffe888a320ac0c1a8bd86af56
Nicholas Poon <nickkpoon@gmail.com>
2021-06-27 21:31:05 -0700

    LAST COMMIT BEFORE FRONT END

--- src/calculator.js
+++ src/calculator.js
@@ -1,69 +1,56 @@
 import React, {useState} from 'react';
 import Parent from "./components/Parent";
 const initialList = [
-
-
   {
     id: 0,
     count: 0,
-    isComplete: true,
   },
   {
     id: 1,
     count: 0,
-    isComplete: false,
   },
   {
     id: 2,
     count: 0,
-    isComplete: false,
   },
   {
     id: 3,
     count: 0,
-    isComplete: false,
   },
   {
     id: 4,
     count: 0,
-    isComplete: false,
   },
   {
     id: 5,
     count: 0,
-    isComplete: false,
   },
   {
     id: 6,
     count: 0,
-    isComplete: false,
   },
   {
     id: 7,
     count: 0,
-    isComplete: false,
   },
   {
     id: 8,
     count: 0,
-    isComplete: false,
   },
   {
     id: 9,
     count: 0,
-    isComplete: false,
   },
   {
     id: 10,
     count: 0,
-    isComplete: false,
   }
 ];
 
 function Calculator() {
 
-  const [click, setClick] = useState(0);
   const [list, setList] = useState(initialList);
+  const [click, setClick] = useState(0);
   const [promotor, setPromotor] = useState(0);
   const [detractor, setDetractor] = useState(0);
   const [neutral, setNeutral] = useState(0);
@@ -101,6 +88,8 @@ function Calculator() {
   }
  
   return (
+
+    
     <ul>
 
       <Parent handleClick={handleClick} />
