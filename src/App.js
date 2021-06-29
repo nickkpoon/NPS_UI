3d0de89222bab5ff69b2cf646d89abc5744c7b6b
Nicholas Poon <nickkpoon@gmail.com>
2021-06-28 22:16:14 -0700

    update readme.md + app.js

--- src/App.js
+++ src/App.js
@@ -3,9 +3,7 @@ import './styles/styles.css';
 import Title from "./components/Title"
 import Calculator from "./components/Calculator";
 import Signup from "./components/Signup";
-import googleIcon from './assets/image-1.png';
-import facebookLogo from './assets/Facebook_f_logo__2019_-1.png';
-import twitterLogo from  './assets/twitter-icon-1.png';
+
 
 export default function App() {
   return (
